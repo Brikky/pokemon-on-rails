185e49f138961d669e51253bce86db8c971e3b01
brikky <brandon.kerr160@topper.wku.edu>
2016-10-23 00:48:13 -0700

    launches to heroku

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,4 +4,9 @@ class Pokemon < ApplicationRecord
 
   has_many :pokemon_groups
   has_many :teams, through: :pokemon_groups
+
+  def self.search(term)
+  where('LOWER(name) LIKE :term OR LOWER(id) LIKE :term', term: "%#{term.downcase}%")
+end
+
 end
